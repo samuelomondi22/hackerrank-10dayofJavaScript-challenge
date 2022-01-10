3c53d8bfc99b51873a7a697c090f97c74038a7b3
samuelomondi22 <omondisam696@gmail.com>
2022-01-10 11:16:31 -0700

    update day 3arrays with comments

--- day3arrays.js
+++ day3arrays.js
@@ -5,8 +5,11 @@
 **/
 function getSecondLargest(nums) {
     // Complete the function
+    // Create a new set of arrays without duplicates [2,3,6,5]
     let num = Array.from(new Set(nums));
+    // arrange the array and remove the largest digit
     num.sort().pop();
+    // remove the second highest digit
     return num.pop();
 }
 
